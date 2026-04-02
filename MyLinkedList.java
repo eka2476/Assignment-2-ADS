9f74d88414c8135a782814df4aaab11819fbb32d
eka2476 <shalkenovaerkezhan@gmail.com>
2026-04-02 13:48:32 +0500

    Refactor MyLinkedList by removing unused methods
    Removed unused methods and cleaned up the MyLinkedList class.

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,6 +1,5 @@
-import java.util.Iterator;
-
 public class MyLinkedList<T> implements MyList<T> {
+    // Inner class for nodes
     private class MyNode {
         T data;
         MyNode next;
@@ -11,34 +10,11 @@ public class MyLinkedList<T> implements MyList<T> {
         }
     }
 
-    private MyNode head;
-    private MyNode tail;
+    private MyNode head;  //main
+    private MyNode tail; // zamikaet
     private int size;
 
-    public MyLinkedList() {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
-    }
-
-    @Override
-    public void add(T item) {
-        addLast(item);
-    }
-
-    @Override
-    public void addLast(T item) {
-        MyNode newNode = new MyNode(item);
-        if (head == null) {
-            head = tail = newNode;
-        } else {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-        }
-        size++;
-    }
-
+    // Adding to the front
     @Override
     public void addFirst(T item) {
         MyNode newNode = new MyNode(item);
@@ -50,71 +26,6 @@ public class MyLinkedList<T> implements MyList<T> {
             head = newNode;
         }
         size++;
-    }
-
-    @Override
-    public void add(int index, T item) {
-        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
-        if (index == 0) {
-            addFirst(item);
-        } else if (index == size) {
-            addLast(item);
-        } else {
-            MyNode current = getNode(index);
-            MyNode newNode = new MyNode(item);
-            newNode.prev = current.prev;
-            newNode.next = current;
-            current.prev.next = newNode;
-            current.prev = newNode;
-            size++;
-        }
-    }
-
-    private MyNode getNode(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        MyNode current = head;
-        for (int i = 0; i < index; i++) {
-            current = current.next;
-        }
-        return current;
-    }
-
-    @Override
-    public T get(int index) {
-        return getNode(index).data;
-    }
-
-    @Override
-    public void remove(int index) {
-        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
-        if (index == 0) {
-            removeFirst();
-        } else if (index == size - 1) {
-            removeLast();
-        } else {
-            MyNode current = getNode(index);
-            current.prev.next = current.next;
-            current.next.prev = current.prev;
-            size--;
-        }
-    }
-
-    @Override
-    public void removeFirst() {
-        if (head == null) return;
-        head = head.next;
-        if (head != null) head.prev = null;
-        else tail = null;
-        size--;
-    }
-
-    @Override
-    public void removeLast() {
-        if (tail == null) return;
-        tail = tail.prev;
-        if (tail != null) tail.next = null;
-        else head = null;
-        size--;
     }
 
     @Override
@@ -131,78 +42,5 @@ public class MyLinkedList<T> implements MyList<T> {
                 current = current.next;
             }
         }
-    }
-
-    @Override
-    public int indexOf(Object object) {
-        MyNode current = head;
-        for (int i = 0; i < size; i++) {
-            if (current.data.equals(object)) return i;
-            current = current.next;
-        }
-        return -1;
-    }
-
-    @Override
-    public int lastIndexOf(Object object) {
-        MyNode current = tail;
-        for (int i = size - 1; i >= 0; i--) {
-            if (current.data.equals(object)) return i;
-            current = current.prev;
-        }
-        return -1;
-    }
-
-    @Override
-    public boolean exists(Object object) {
-        return indexOf(object) != -1;
-    }
-
-    @Override
-    public void clear() {
-        head = tail = null;
-        size = 0;
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public T getFirst() { return (head != null) ? head.data : null; }
-
-    @Override
-    public T getLast() { return (tail != null) ? tail.data : null; }
-
-    @Override
-    public void set(int index, T item) {
-        getNode(index).data = item;
-    }
-
-    @Override
-    public Object[] toArray() {
-        Object[] array = new Object[size];
-        MyNode current = head;
-        for (int i = 0; i < size; i++) {
-            array[i] = current.data;
-            current = current.next;
-        }
-        return array;
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            private MyNode current = head;
-            @Override
-            public boolean hasNext() { return current != null; }
-            @Override
-            public T next() {
-                T data = current.data;
-                current = current.next;
-                return data;
-            }
-        };
     }
 }
