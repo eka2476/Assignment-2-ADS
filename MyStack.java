45b6c42c641ef377e6d40ea270acbf857692a142
eka2476 <shalkenovaerkezhan@gmail.com>
2026-04-09 11:29:45 +0500

    Add comments to MyStack methods

--- MyStack.java
+++ MyStack.java
@@ -1,21 +1,21 @@
 public class MyStack<T> {
     private MyArrayList<T> list = new MyArrayList<>();
 
-    public void push(T item) {
+    public void push(T item) {  //Push it to the limit
         list.addLast(item);
     }
 
-    public T pop() {
+    public T pop() {       // Забираем последний элемент
         T item = list.getLast();
         list.removeLast();
         return item;
     }
 
-    public T peek() {
+    public T peek() {      //Чисто посмотреть
         return list.getLast();
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty() {     // Проверка на пустоту
         return list.size() == 0;
     }
 
