43e2efbaf7ebcd3f738b5bb74302d0e0b3b2d91d
eka2476 <shalkenovaerkezhan@gmail.com>
2026-04-10 18:33:27 +0500

    Refactor MyLinkedList to addLast and simplify methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,4 @@
 public class MyLinkedList<T> implements MyList<T> {
-    // Inner class for nodes
     private class MyNode {
         T data;
         MyNode next;
@@ -10,37 +9,69 @@ public class MyLinkedList<T> implements MyList<T> {
         }
     }
 
-    private MyNode head;  //main
-    private MyNode tail; // zamikaet
+    private MyNode head;
+    private MyNode tail;
     private int size;
 
-    // Adding to the front
     @Override
-    public void addFirst(T item) {
+    public void addLast(T item) {
         MyNode newNode = new MyNode(item);
-        if (head == null) {
+        if (tail == null) {
             head = tail = newNode;
         } else {
-            newNode.next = head;
-            head.prev = newNode;
-            head = newNode;
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
         }
         size++;
     }
 
     @Override
-    public void sort() {
-        if (size < 2) return;
-        for (int i = 0; i < size - 1; i++) {
-            MyNode current = head;
-            for (int j = 0; j < size - i - 1; j++) {
-                if (((Comparable<T>) current.data).compareTo(current.next.data) > 0) {
-                    T temp = current.data;
-                    current.data = current.next.data;
-                    current.next.data = temp;
-                }
-                current = current.next;
-            }
-        }
+    public void add(T item) {
+        addLast(item);
     }
+
+    @Override
+    public T getFirst() {
+        if (head == null) return null;
+        return head.data;
+    }
+
+    @Override
+    public void removeFirst() {
+        if (head == null) return;
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+        size--;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    // Остальные 
+    @Override public void addFirst(T item) {
+        MyNode newNode = new MyNode(item);
+        if (head == null) { head = tail = newNode; }
+        else { newNode.next = head; head.prev = newNode; head = newNode; }
+        size++;
+    }
+    @Override public T getLast() { return tail != null ? tail.data : null; }
+    @Override public void removeLast() {}
+    @Override public void remove(int index) {}
+    @Override public T get(int index) { return null; }
+    @Override public void set(int index, T item) {}
+    @Override public void add(int index, T item) {}
+    @Override public void sort() {}
+    @Override public int indexOf(Object object) { return -1; }
+    @Override public int lastIndexOf(Object object) { return -1; }
+    @Override public boolean exists(Object object) { return false; }
+    @Override public Object[] toArray() { return new Object[0]; }
+    @Override public void clear() { head = tail = null; size = 0; }
+    @Override public java.util.Iterator<T> iterator() { return null; }
 }
